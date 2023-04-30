7e4a900a0b83ac09d07b055603c1500619c18a73
PranjalAnilKunde <93993670+PranjalAnilKunde@users.noreply.github.com>
2023-04-30 21:30:49 +0530

    Add files via upload

---
+++ Assign 3_angular/src/app/signup/signup.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  constructor(private router:Router){}
+  display(){
+    this.router.navigate(["login"])
+  }
+}
